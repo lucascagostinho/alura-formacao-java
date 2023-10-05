e4b90ea412ae7a72eacdcb3c775886dcdd950a34
Lucas Corrêa Agostinho <lucascagostinho@gmail.com>
2023-10-04 23:58:37 -0300

    Trabalhando com variaveis

--- firstprogram/src/Main.java
+++ firstprogram/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Movie: Star Wars");
+
+        int year = 2000;
+        int year2;
+        year2 = 2001;
+        int releaseYear = 2002;
+
+        System.out.println(year);
+
+         //Concatenação
+        System.out.println("Ano de lançamento: " + year);
+        System.out.println("Ano de lançamento: " + year2);
+        System.out.println("Ano de lançamento: " + releaseYear);
+
+
+         /*
+         Variáveis do tipo inteiro não armazenam valores decimais
+         int filmNote = 8.1;
+         */
+        double filmNote = 8.1;
+
+        /*
+        Mas tipo double armazenam valores inteiros
+        */
+        double filmNote2 = 8;
     }
 }
