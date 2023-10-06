98bc179461fae2978ae2590c70fba55bb94f8936
Lucas Corrêa Agostinho <lucascagostinho@gmail.com>
2023-10-06 00:06:11 -0300

    Utilizacao de String

---
+++ firstprogram/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+@
+firstprogram.iml,7\f\7f045cfda02b4c1e05663922132d5c1cfcfe6a9a
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
--- firstprogram/src/Main.java
+++ firstprogram/src/Main.java
@@ -26,5 +26,41 @@ public class Main {
         Mas tipo double armazenam valores inteiros
         */
         double filmNote2 = 8;
+
+        //Precedência
+        double average = (9.7 + 6.2 + 8) / 3;
+        System.out.println(average);
+
+        String synopsi;
+        synopsi = "Filme dos anos 2000";
+        System.out.println(synopsi);
+
+        String name = "Lucas";
+        String message = "Olá ";
+        String message2 = message + name + "!";
+        System.out.println(message2);
+
+        //Text Blocks, disponível a partir da versão 15 do java
+        String synopsi2 = """
+                Filme legal
+                No espaço
+                Bom
+                Ano de lançamento
+                """ + releaseYear;
+        System.out.println(synopsi2);
+
+        String password = "12345";
+        String password2 = "1234";
+        if (password.equals("12345")) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        if (password.equals("password2")) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
